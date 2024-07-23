67f314c399984c17e1b02e05d842f98e6a3c5e24
yerrrim <yerim629@ajou.ac.kr>
2024-07-23 13:14:26 +0900

    1단계 하는 중

--- src/App.js
+++ src/App.js
@@ -2,12 +2,16 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const name = "윤예림";
+  const email = "yerim629";
+  const age = 25;
+
   return (
     <div className="App">
-      <header className="App-header">
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> 아주대학교
         </p>
         <a
           className="App-link"
@@ -17,7 +21,19 @@ function App() {
         >
           Learn React
         </a>
+      </header> */}
+      <header>
+        <h3>본인 소개 페이지</h3>
       </header>
+      <main>
+        <div>
+          <ul>
+            <li>이름: <span>{name}</span></li>
+            <li>이메일: <span>{email}</span></li>
+            <li>나이: <span>{age}</span></li>
+          </ul>
+        </div>
+      </main>
     </div>
   );
 }
