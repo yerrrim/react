bef81f6f5e6882e032f09371c3448a1023aaca09
yerrrim <yerim629@ajou.ac.kr>
2024-07-29 04:08:13 +0900

    1단계-이름

--- src/App.js
+++ src/App.js
@@ -1,38 +1,48 @@
 import logo from './logo.svg';
 import './App.css';
+import {useState} from "react";
 
 function App() {
-  const name = "윤예림";
-  const email = "yerim629";
-  const age = 25;
+  const [userInfo, setUserInfo] = useState({
+    name: '홍길동'
+  })
+  const [isEditing, setIsEditing] = useState({
+    name: false,
+  })
 
+  const handleChange = (field, value) => {
+    setUserInfo((prev)=>({...prev, [field]: value}));
+  }
+
+  const handleEditToggle = (field) => {
+    setIsEditing((prev)=>({...prev, [field]:!prev[field]}))
+  }
+  
   return (
-    <div className="App">
-      {/* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> 아주대학교
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header> */}
+    <div>
       <header>
         <h3>본인 소개 페이지</h3>
       </header>
       <main>
         <div>
-          <ul>
-            <li>이름: <span>{name}</span></li>
-            <li>이메일: <span>{email}</span></li>
-            <li>나이: <span>{age}</span></li>
-          </ul>
+          <label>이름: </label>
+          {isEditing.name ? (
+            <input value={userInfo.name}
+            onChange={(e)=>{handleChange('name', e.target.value);
+            }}/>
+          ):(
+            <span>{userInfo.name}</span>
+          )}
+          <button onClick={()=>handleEditToggle('name')}>{isEditing.name ? '저장':'수정'}</button>
         </div>
+        {/* <div>
+          <label>이름: </label>
+          <input value={userInfo.name}
+          onChange={(e)=>{
+            return handleChange('name', e.target.value);
+          }}/>
+          <button>수정</button>
+        </div> */}
       </main>
     </div>
   );
